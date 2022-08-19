faa84bb2abf9d3ad918c6e7c870f9136a7b2b610
Deepa145 <Deepa.ravi@chainsys.com>
2022-08-19 16:12:32 +0530

    Final Project Commit

--- Book.sql
+++ Book.sql
@@ -13,7 +13,7 @@ select * from book;
 insert into book(bookId,bookName,dateOfPublishing,category,price,stockInHand,sales) 
 values(501,'Theentamil','09/08/2001','Tamil',100,10,1);
 
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
 create table authors(authorId number(30)not null,
 	authorName varchar(30) not null,
 	authorAddress varchar(60) not null,
@@ -27,7 +27,7 @@ insert into authors(authorId,authorName,authorAddress,phoneNumber,email,gender) 
 
 Select * from authors;
 
-----------------------------------------------------------------------------------------------
+---------------------------------------------
     
 create table shop(shopId number(30) not null,
 	shopLocation varchar(50)not null,
@@ -38,18 +38,16 @@ create table shop(shopId number(30) not null,
      insert into shop(shopId,shopLocation,contactPerson,phoneNumber) values(1001,'Madurai','Gopika',6789054312);
      
      select * from shop;
-------------------------------------------------------------------------------------------------
+-------------------------------------------------
      
      
 create table authorbookdetails(
 authorId number(30) not null,
 	bookId number(30) not null,
-	royalty	varchar(80) not null,
+	royalty	number(30) not null,
      primary key(authorId,bookId),
     foreign key (authorId) references authors(authorId),
     foreign key (bookId) references book(bookId));
-
------------------------------------------------------------------------------------------------------
     
     insert into authorbookdetails(authorId,bookId,royalty) values(111,501,'Excellent');
     insert into authorbookdetails(authorId,bookId,royalty) values(112,502,'Good');
@@ -57,7 +55,9 @@ authorId number(30) not null,
     
     select * from authorbookdetails;
     desc authorbookdetails;
-------------------------------------------------------------------------------------------------------
+    alter table authorbookdetails modify column royalty double not null;
+    drop table authorbookdetails;
+ ----------------------------------------   
 create table orderedhistory(
 	orderedId number(30) not null,
 	status varchar(30) not null,
@@ -66,13 +66,12 @@ create table orderedhistory(
 	totalAmount number(35) not null,
      primary key (orderedId),
       foreign key (shopId) references shop(shopId));
------------------------------------------------------------------------------------------------------------
       
        insert into orderedhistory values(2001,'Delivered',1001,'07/08/2000',100);
       
       select * from orderedhistory;
+  ---------------------------------------------    
       
--------------------------------------------------------------------------------------------------------------
  create table orderdetails(
 	orderedId number(30) not null,
 	bookId number(30) not null, 
@@ -81,13 +80,30 @@ create table orderedhistory(
     primary key(orderedId,bookId),
      foreign key (orderedId) references orderedhistory(orderedId),
       foreign key (bookId) references book(bookId));
------------------------------------------------------------------------------------------------------------
+      
       insert into orderdetails(orderedId,bookId,quantity,amount) values(2001,501,1,100);
       select * from orderdetails;
-      
-COMMIT;
+ ------------------------------------------------     
 
 create table admin(userName varchar(30) not null,passWord varchar(30) not null);
 
 insert into admin values('Deepa','Deepa@123');
 select * from admin;
+-----------------------------------------------------
+COMMIT;
+-------------------------------------------------
+create sequence boid start with 501 increment by 1;
+drop sequence boid;
+
+create sequence auid start with 110 increment by 1;
+
+create sequence shid start with 1000 increment by 1;
+
+create sequence odid start with 2000 increment by 1;
+
+
+
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,93 @@
+
+# Title : Author Book Publishing and Book Sales
+
+
+# Description
+
+Author Book Publishing and Book Sales :
+
+Author Book Publishing and Book Sales is a Book publishing and sales system when Admin can get the information about the orders of book ,
+book sales and Royalty of booksales process.Book publishers oversee all aspects of the book production process for a publishing firm or 
+an independent book publisher. In this career, you select authors and commission books and ebooks, and negotiate contracts with the authors,
+and track the writing process so that the author meets publishing deadline.
+
+
+# General Information
+
+1.Author Book Publishing and Book Sales is used for getting data about royalty for authors from ordered books.
+
+2.AuthorBook Details describes the authors royalty to fixed the every authors.
+
+3.Order details shows the book order from the available ordered Id and Shop Id.
+
+# Project Flow
+
+  HOME ---> -ADMIN LOGIN  
+               
+
+                ALL BOOKS DETAILS---> - 
+					ADD BOOKS
+					VIEW SPECIFIC BOOK
+					VIEW BOOK & AUTHOR'S ROYALTY                
+                                   
+              ALL AUTHORS DETAILS---> -
+                                      - ADD NEW AUTHOR
+                                      - VIEW SPECIFIC AUTHOR DETAILS
+
+      ALL AUTHORS ROYALTY DETAILS---> -
+                                      - AUTHOR'S ROYALTY LIST
+				      - ADD NEW ROYALTY FOR AUTHORS
+                                      - VIEW AUTHOR ROYALTY FOR BOOK
+				      - REMOVE AUTHOR BOOK ROYALTY
+				      - UPDATE AUTHOR BOOK ROYALTY
+				      - VIEW AUTHOR,BOOK AND ROYALTY DETAILS
+
+                SELL BOOK DETAILS---> - 
+                                      - SOLD BOOK LIST
+                                      - SELL BOOK 
+				      - VIEW SOLD BOOK DETAILS
+                                      - UPDATE SOLD BOOK
+				      - REMOVE SOLD BOOK DETAILS
+
+       ALL ORDERS HISTORY DETAILS---> - 
+				      - ALL ORDERED HISTORY LIST
+                                      - ADD NEW ORDERED HISTORY 
+                                      - VIEW ORDERED HISTORY DETAILS
+				      - VIEW ORDEREDHISTORY AND SOLD BOOK DETAILS 
+                                      - VIEW ORDEREDHISTORY AND ORDERED SHOP DETAILS
+				      
+	        ALL SHOP DETAILS ---> -  
+                                      - AVAILABLE SHOP LIST
+                                      - ADD NEW SHOP 
+				      - VIEW SPECIFIC SHOP DETAILS 
+                                      
+# Technology
+
+# Frontend 
+
+1.CSS
+2.HTML
+3.JAVASCRIPT
+
+# Backend
+
+1. Java Version:1.8
+2.SpringBoot: 2.6.7
+
+# Database
+
+1. Oracle(SQL)                                             
+# Features
+
+1.DB Connectivity
+2.DB Relational data Handling.
+   Lookup relation.
+   One to many relation.
+3.Form validation in frontend level.
+4.Form validation in Javascript level.
+5.Form null validation in Oracle database level.
+readme.txt
+Displaying readme.txt.
+
+
+
